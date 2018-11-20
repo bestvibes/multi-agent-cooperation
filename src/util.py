5c79161cfc9a6f9db6a579adc7c6c0e0f211c21a
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2018-11-20 14:50:57 -0800

    reward: add chasing reward function with obstacles

---
+++ src/util.py
@@ -0,0 +1,11 @@
+import math
+
+def get_coordinates_l2_dist(c1: tuple, c2: tuple):
+    if len(c1) != len(c2):
+        raise ValueError("Coordinate dimension mismatch!")
+
+    num_dims = len(c1)
+    dim_dists = map(lambda i: c1[i] - c2[i], range(num_dims))
+    l2_dist = math.sqrt(sum(map(lambda d: d**2, dim_dists)))
+
+    return l2_dist
