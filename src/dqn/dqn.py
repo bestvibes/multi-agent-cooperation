45c9f1c8db6625197b53995f44ef12a81344f996
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2018-11-29 15:39:17 -0800

    dqn: clean up code

--- src/dqn/dqn.py
+++ src/dqn/dqn.py
@@ -1,19 +1,10 @@
+from collections import namedtuple
+
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from collections import namedtuple
-from replay_memory import ReplayMemory
-
-# BATCH_SIZE = 128
-# GAMMA = 0.999
-# EPS_START = 0.9
-# EPS_END = 0.05
-# EPS_DECAY = 200
-# TARGET_UPDATE = 10
-
-# Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 class DQN(nn.Module):
     def __init__(self):
--- src/dqn/main.py
+++ src/dqn/main.py
@@ -1,82 +1,82 @@
 import copy
 
-import pytorch
+import torch
 
 from src.replay_memory import ReplayMemoryPusher
-from src.dqn import DQN
+from src.dqn.dqn import DQN
 from src.dqn.optimize_model import ComputeLoss
 from src.dqn.select_action_dqn import select_action_dqn
 
 def dqn_trainer(initial_env,
-				start_state: tuple,
-				save_path: str,
-				learning_rate: float=1e-4,
-				batch_size: int=128,
-				gamma: float=0.999,
-				memory_capacity: int=1000,
-				max_training_steps: int=1000,
-				max_episode_steps: int=25,
-				epsilon: int=0.1,
-				target_network_update_interval_steps: int=10):
+                start_state: tuple,
+                save_path: str,
+                learning_rate: float=1e-4,
+                batch_size: int=128,
+                gamma: float=0.999,
+                memory_capacity: int=1000,
+                max_training_steps: int=1000,
+                max_episode_steps: int=25,
+                epsilon: int=0.1,
+                target_network_update_interval_steps: int=10):
 
-	# Initialization
-	memory = []
-	replay_memory_pusher = ReplayMemoryPusher(memory_capacity)
+    # Initialization
+    memory = []
+    replay_memory_pusher = ReplayMemoryPusher(memory_capacity)
 
-	policy_net = DQN()
-	target_net = DQN()
-	target_net.load_state_dict(policy_net.state_dict())
-	target_net.eval()
+    policy_net = DQN()
+    target_net = DQN()
+    target_net.load_state_dict(policy_net.state_dict())
+    target_net.eval()
 
     optimizer = torch.optim.Adam(policy_net.parameters())
     loss_function = ComputeLoss(batch_size, gamma)
 
     training_step = 0
     while(training_step <= max_training_steps):
-    	# reset env
-    	env = copy.deepcopy(initial_env)
+        # reset env
+        env = copy.deepcopy(initial_env)
 
-    	state = start_state
-    	episode_step = 0
+        state = start_state
+        episode_step = 0
         while(episode_step <= max_episode_steps):
-    		action = select_action_dqn(state, policy_net, epsilon)
+            action = select_action_dqn(state, policy_net, epsilon)
 
-    		next_state, reward, done = env(action)
+            next_state, reward, done = env(action)
 
-    		# store the transition in memory
-    		memory = replay_memory_pusher(memory, state, action, next_state, reward)
+            # store the transition in memory
+            memory = replay_memory_pusher(memory, state, action, next_state, reward)
 
-			# optimize model
-			loss = loss_function(memory, policy_net, target_net)
-			optimizer.zero_grad()
-			loss.backward()
-			optimizer.step()
+            # optimize model
+            loss = loss_function(memory, policy_net, target_net)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-			if done: break
+            if done: break
 
-			state = next_state
-			episode_step += 1
+            state = next_state
+            episode_step += 1
 
-		# update target network
-		if (training_step % target_network_update_interval_steps == 0):
-			target_net.load_state_dict(policy_net.state_dict())
+        # update target network
+        if (training_step % target_network_update_interval_steps == 0):
+            target_net.load_state_dict(policy_net.state_dict())
 
-		training_step += 1
+        training_step += 1
 
-	torch.save(policy_net.state_dict(), save_path)
+    torch.save(policy_net.state_dict(), save_path)
 
 def dqn_runner(env,
-				start_state: tuple,
-				renderer: callable,
-				load_path: str,
-				max_running_steps: int=25,
-				epsilon: int=0.1):
+                start_state: tuple,
+                renderer: callable,
+                load_path: str,
+                max_running_steps: int=25,
+                epsilon: int=0.1):
 
-	policy_net = DQN()
-	policy_net.load_state_dict(torch.load(load_path))
-	policy_net.eval()
+    policy_net = DQN()
+    policy_net.load_state_dict(torch.load(load_path))
+    policy_net.eval()
 
-	state = start_state
+    state = start_state
 
     renderer(state)
     for i in range(0, max_running_steps):
