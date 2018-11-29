6ee5b2e45885125a49e830c9b9f71a9aa555f5a3
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2018-11-29 14:56:21 -0800

    replay_memory: fix tests

--- tests/test_replay_memory.py
+++ tests/test_replay_memory.py
@@ -1,21 +1,24 @@
 import unittest
+from collections import namedtuple
 
 import src.replay_memory
+
+TestType = namedtuple('Test', ('value'))
 
 class TestReplayMemoryPusher(unittest.TestCase):
     def test_push_to_capacity(self):
         replay_memory = []
         capacity = 5
-        pusher = src.replay_memory.ReplayMemoryPusher(capacity)
+        pusher = src.replay_memory.ReplayMemoryPusher(TestType, capacity)
         for i in range(capacity):
             replay_memory = pusher(replay_memory, i)
 
-        self.assertEqual([0, 1, 2, 3, 4], replay_memory)
+        self.assertEqual(list(map(TestType, [0, 1, 2, 3, 4])), replay_memory)
 
     def test_push_over_capacity(self):
         replay_memory = [10, 20, 30, 40, 50]
         capacity = 5
-        pusher = src.replay_memory.ReplayMemoryPusher(capacity)
+        pusher = src.replay_memory.ReplayMemoryPusher(TestType, capacity)
         for i in range(capacity):
             replay_memory = pusher(replay_memory, i)
-            self.assertTrue(i in replay_memory and len(replay_memory) == capacity)
+            self.assertTrue(TestType(i) in replay_memory and len(replay_memory) == capacity)
