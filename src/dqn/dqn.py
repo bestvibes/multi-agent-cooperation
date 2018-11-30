6e532ffcc3363a6df4c8d17dc19af9027567adc9
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2018-11-29 16:23:02 -0800

    dqn: fix missed merge conflict

--- src/dqn/dqn.py
+++ src/dqn/dqn.py
@@ -1,29 +1,7 @@
-<<<<<<< HEAD
-# import sys
-# sys.path.append('../')
-=======
-from collections import namedtuple
->>>>>>> 562e9e7986f34d2a46a6786b46637393ae4a172f
-
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-<<<<<<< HEAD
-from collections import namedtuple
-# from src.replay_memory import ReplayMemoryPusher
-
-# BATCH_SIZE = 128
-# GAMMA = 0.999
-# EPS_START = 0.9
-# EPS_END = 0.05
-# EPS_DECAY = 200
-# TARGET_UPDATE = 10
-
-# Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
-=======
->>>>>>> 562e9e7986f34d2a46a6786b46637393ae4a172f
 
 class DQN(nn.Module):
     def __init__(self):
