8f6aeda763c4096c46ece0488be40aac8c95f080
besth <zhaoqy1997@outlook.com>
2019-01-17 19:21:52 -0800

    added policy net for policy gradient

---
+++ src/policy_gradient/policy_net.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PolicyNet(nn.Module):
+    def __init__(self):
+        super(PolicyNet, self).__init__()
+        self.linear1 = nn.Linear(4, 64)
+        self.linear2 = nn.Linear(64, 64)
+        self.linear3 = nn.Linear(64, 64)
+        self.linear4 = nn.Linear(64, 4)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = F.relu(self.linear3(x))
+        x = self.linear4(x)
+        return x
